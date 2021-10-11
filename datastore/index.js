47d04a00bb4861c4b51eb4dacddba4a909347ed1
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-11 16:13:17 -0700

    Completed delete todo

--- datastore/index.js
+++ datastore/index.js
@@ -48,14 +48,14 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var fileDir = path.join(exports.dataDir, `${id}.txt`);
+  fs.unlink(fileDir, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
